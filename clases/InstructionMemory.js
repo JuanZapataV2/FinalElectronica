29b268cbddd0927f3d1e92071ef4f1352987b8ce
Isabel Vallejo Gomez <64813658+IsabelVallejoUam@users.noreply.github.com>
2022-05-10 19:45:55 -0500

    a
    a

--- clases/InstructionMemory.js
+++ clases/InstructionMemory.js
@@ -1,5 +1,21 @@
-class InstructionMemory{
-    constructor(){
-        
+class InstructionMemory {
+    constructor() {
+
+    }
+
+    showNextInstruction(instruction) {
+        let new_inst = document.createElement("div");
+        new_inst.classList.add("row");
+
+        let row = document.createElement("div");
+        row.classList.add("col-sm", "border", "border-5");
+
+        let text = document.createElement("span");
+        text.appendChild(document.createTextNode(instruction));
+
+        row.appendChild(text);
+        new_inst.appendChild(row);
+
+        document.getElementById('inst_memory').appendChild(new_inst)
     }
 }
